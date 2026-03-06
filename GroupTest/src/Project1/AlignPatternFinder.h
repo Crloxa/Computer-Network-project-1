32b47b6546762ed92248e4516a79bf535e634e6f
lifuyue <l1fuyue@icloud.com>
2026-03-06 17:52:10 +0800

    重整根目录，同时引入宋润涵等团队示例

---
+++ GroupTest/src/Project1/AlignPatternFinder.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "opencv2/opencv.hpp"
+#include <vector>
+using namespace cv;
+using namespace std;
+
+struct AlignmentPattern
+{
+	Point2d position;
+	int count;
+	double estimatedModuleSize;
+	AlignmentPattern():count(1) {};
+	AlignmentPattern(double posX, double posY, double estimatedModuleSize)
+		:count(1), position(posX, posY), estimatedModuleSize(estimatedModuleSize) {};
+	bool AboutEquals(double moduleSize, double i, double j)
+	{
+		if (abs(i - position.y) <= moduleSize && abs(j - position.x) <= moduleSize)
+		{
+			double moduleSizeDiff = abs(moduleSize - estimatedModuleSize);
+			return moduleSizeDiff <= 1.0 || moduleSizeDiff / estimatedModuleSize <= 1.0;
+		}
+		return false;
+	}
+};
+
+class AlignmentPatternFinder
+{
+public:
+	AlignmentPatternFinder();
+	AlignmentPattern* Find(Mat image, int startX, int startY, int width, int height, double moduleSize);
+private:
+	Mat image;
+	int crossCheckStateCount[5] = { 0,0,0,0,0 };
+	vector<AlignmentPattern> possibleCenters;
+	double moduleSize;
+	AlignmentPattern* HandlePossibleCenter(int statecount[], int i, int j);
+	double CrossCheckVertical(int startI, int centerJ, int maxCount, int originalStateCountTotal);
+	bool FoundPatternCross(int statecount[]);
+	double CenterFromEnd(int stateCount[], int end);
+};
---
+++ GroupTest/src/Project1/源.cpp
@@ -0,0 +1,50 @@
+﻿#include <cstdio>
+#include "detector.h"
+#include "opencv2/opencv.hpp" //opencv 的头文件
+#include "slicer.h"
+#include "FileToVedio.h"
+#include "FileToMp4.h"
+#include <bitset>
+using namespace std;
+
+using namespace cv; //opencv 的命名空间
+
+
+int main()
+{
+	/*FileToVedio a(R"(C:\Users\yuyua\Desktop\e1.bin)");
+	a.GenerateVedio(R"(D:\QR_Code\QR_Code.mp4)", 1080, 1920, 10);*/
+	
+	/*FileToMp4 a;
+	a.MakePictureSequence();*/
+	int start = getTickCount();
+
+	Mat imgGray, res;
+	Mat img;
+	VedioSlicer DT(R"(C:\Users\yuyua\Desktop\VID_20200317_142535.mp4)");
+	int i = 0;
+	while (1)
+	{
+		++i;
+		img = DT.GetNextFrame();
+		//if (i % 3 != 1)continue;
+		cout << i << ' ';
+		//img = imread(R"(D:\QR_Code\Code10_trans.png)");
+		if (img.empty())
+		{
+			break;
+		}
+		//imwrite(".//1.jpg", res);
+ 		Detector d(img, 108, 192, 10);
+		if (!d.Detect())throw;
+		/*char x[2];
+		d.picCount(x);
+		cout << (int)x[0] << (int)x[1] << endl;*/
+		//d.GetBinaryData();
+	}
+
+	int end = getTickCount();
+	int last = end - start;
+	cout << "time consume: " << last / getTickFrequency() << 's' << endl;
+	return 0;
+}
